8820bf1e2c522aeab869183b1ec61b109a6593be
Genaro Madrid <genaro@appio.mx>
2015-07-13 17:55:50 -0500

    move url management to Connection

--- lib/webbtc/connection.rb
+++ lib/webbtc/connection.rb
@@ -2,25 +2,36 @@ module Webbtc
 
   class Connection
 
-    class << self
-      def get(path, payload={})
-        query :get, path, payload
-      end
-
-      def post(path, payload={})
-        query :post, path, payload
-      end
-
-      def query(method, path, payload={})
-        uri = Webbtc.endpoint_uri(path)
-        begin
-          response = RestClient::Request.execute(:method => method, :url => uri, :payload => payload, :ssl_version => 'SSLv23')
-        rescue Exception => e
-          response = e.response
-        end
-        JSON.parse response, :symbolize_names => true
-      end
+    def initialize(url)
+      @url = url
     end
+
+    def get(path, payload={})
+      query :get, path, payload
+    end
+
+    def post(path, payload={})
+      query :post, path, payload
+    end
+
+    def query(method, path, payload={})
+      uri = endpoint_uri(path)
+      begin
+        response = RestClient::Request.execute(:method => method, :url => uri, :payload => payload, :ssl_version => 'SSLv23')
+      rescue Exception => e
+        response = e.response
+      end
+      JSON.parse response, :symbolize_names => true
+    end
+
+    private
+
+      def endpoint_uri(path='')
+        if path[0] == '/'
+          path[0] = ''
+        end
+        "#{@url}/#{path}"
+      end
 
   end
 
